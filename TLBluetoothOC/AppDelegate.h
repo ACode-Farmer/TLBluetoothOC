030864405157f2a88c4ab20f34ea35c40830c523
CodeFarmer <wang.jie@ve-link.com>
2020-10-29 11:09:15 +0800

    Initial Commit

---
+++ TLBluetoothOC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TLBluetoothOC
+//
+//  Created by Will on 2020/10/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
